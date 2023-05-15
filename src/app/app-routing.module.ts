9ef4979ac5d206a700b376863768341416e44b70
TheRemG <ricardoe200315@gmail.com>
2023-05-14 20:30:42 -0400

    Subir publciacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CrearPubComponent } from './crear-pub/crear-pub.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {
+    path: '',
+    component: NavbarComponent
+  },
+  {
+    path: 'crear-pub',
+    component: CrearPubComponent
+  }
+];
+
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { CrearPubComponent } from './crear-pub/crear-pub.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    CrearPubComponent
   ],
   imports: [
     BrowserModule,

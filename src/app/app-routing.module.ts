8b2bdbf5c2c5b5c851eec23e9eb40e85a080ea2c
TheRemG <ricardoe200315@gmail.com>
2023-06-13 15:48:50 -0400

    Logo, recuperar contraseña, tema seleccionable

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CrearPubComponent } from './crear-pub/crear-pub.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
 
@@ -13,6 +14,10 @@ const routes: Routes = [
   {
     path: 'crear-pub',
     component: CrearPubComponent
+  },
+  {
+    path:'login',
+    component: LoginComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { CrearPubComponent } from './crear-pub/crear-pub.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CrearPubComponent
+    CrearPubComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
